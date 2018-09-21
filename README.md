c62f2a2a69fd10313245b510c09e03082a06e841
Maksim Sinik <maksim@sinik.it>
2018-09-21 18:16:00 +0200

    removes lib from project

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # interference-fastify-plugin
 A fastify plugin that knows how to handle Interference errors
+
+## REF
+[interference](https://www.npmjs.com/package/interference)
--- lib/index.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="fastify" />
-export declare const errorHandler: (error: any, _: any, reply: any) => void;
-declare const _default: import("fastify").Plugin<{}, {}, {}, {}>;
-export default _default;
