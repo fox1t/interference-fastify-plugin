8f51208c67ab69eb2cddd2d7f952465d57912f93
Maksim Sink <maksim@sinik.it>
2019-06-20 17:37:04 +0200

    Updates code to support Fastify 2.x.x

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,33 @@
 import fp from 'fastify-plugin'
+import { FastifyError } from 'fastify'
 
-export const errorHandler = (error: any, req, reply) => {
-  if (error.validation) {
-    error.type = 'DOCUMENT_VALIDATION_ERROR'
-    error.details = error.validation
-  }
-  const statusCode = error.statusCode || 500
-  reply.code(statusCode)
-
-  const response =
-    statusCode !== 500
-      ? {
-          error: error.type,
-          message: error.message,
-          statusCode,
-          details: error.details,
-        }
-      : {
-          error: error.type,
-          message: 'Something went wrong. Try again later.',
-          statusCode,
-        }
-
-  reply.send(response)
-}
+type InterferenceError = FastifyError & { type: string; details: unknown }
 
 export default fp(function protovisionPlugin(fastify, _, next) {
-  fastify.setErrorHandler(errorHandler)
+  fastify.setErrorHandler(function(error: InterferenceError, req, reply) {
+    if (error.validation) {
+      error.statusCode = 400
+      error.type = 'DOCUMENT_VALIDATION_ERROR'
+      error.details = error.validation
+    }
+    const statusCode = error.statusCode || 500
+
+    req.log.error(error)
+    const response =
+      statusCode !== 500
+        ? {
+            error: error.type,
+            message: error.message,
+            statusCode,
+            details: error.details,
+          }
+        : {
+            error: error.type,
+            message: 'Something went wrong. Try again later.',
+            statusCode,
+          }
+
+    reply.code(statusCode).send(response)
+  })
   next()
 })
