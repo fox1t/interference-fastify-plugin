e0cd571c4ee260a843ade2d4a50272d576ede575
greguz <gregolettogiacomo@gmail.com>
2020-05-27 16:30:12 +0200

    Moves codes mapping inside fastify instance

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
-import { FastifyRequest, FastifyReply } from 'fastify'
-import { ServerResponse } from 'http'
+import { FastifyInstance, FastifyRequest, FastifyReply } from 'fastify'
 import fp from 'fastify-plugin'
+import { isInterference } from 'interference'
 
-function errorHandler(error: any, request: FastifyRequest, reply: FastifyReply<ServerResponse>) {
-  const statusCode = error.validation ? 400 : reply.httpCodes[error.type] || error.statusCode || 500
+function errorHandler(
+  this: FastifyInstance,
+  error: any,
+  request: FastifyRequest,
+  reply: FastifyReply<any>,
+) {
+  const statusCode: number = isInterference(error)
+    ? error.statusCode || this.interference.get(error.type) || 500
+    : error.validation
+    ? 400
+    : 500
+
   const isClientError = statusCode >= 400 && statusCode < 500
 
   if (isClientError) {
@@ -12,43 +22,46 @@ function errorHandler(error: any, request: FastifyRequest, reply: FastifyReply<S
     request.log.error({ ...error, statusCode })
   }
 
-  const response = isClientError
-    ? {
-        error: error.type,
-        message: error.message,
-        statusCode,
-        details: error.validation || error.details,
-      }
-    : {
-        error: error.type,
-        message: 'Something went wrong. Try again later.',
-        statusCode,
-      }
+  let response: any
+  if (isInterference(error) && isClientError) {
+    response = {
+      error: error.type,
+      message: error.message,
+      details: error.details,
+      statusCode,
+    }
+  } else if (error.validation) {
+    response = {
+      error: 'INVALID_REQUEST',
+      message: error.message,
+      details: error.validation,
+      statusCode,
+    }
+  } else {
+    response = {
+      error: 'INTERNAL_ERROR',
+      message: 'An error occurred',
+      details: {},
+      statusCode,
+    }
+  }
 
   reply.code(statusCode).send(response)
 }
 
-interface Codes {
-  [key: string]: number
-}
-
-export interface InterferencePluginOptions {
-  codes?: Codes
-}
-
 export default fp(
-  async function interferencePlugin(fastify, opts: InterferencePluginOptions) {
-    fastify.setErrorHandler(errorHandler)
-    fastify.decorateReply('httpCodes', opts.codes ?? {})
+  async function interferencePlugin(fastify) {
+    fastify.decorate('interference', new Map<string, number>())
+    fastify.setErrorHandler(errorHandler.bind(fastify))
   },
   {
     name: 'interference-fastify-plugin',
     fastify: '^2.0.0',
   },
 )
+
 declare module 'fastify' {
-  type HttpResponse = ServerResponse
-  interface FastifyReply<HttpResponse> {
-    httpCodes: Codes
+  interface FastifyInstance {
+    interference: Map<string, number>
   }
 }
